8c7bdb7621374b94b6211e6d8a24ca380773b668
ibrahimvau5518 <jibonm676@gmail.com>
2025-12-06 10:56:44 +0600

    install & import DaisyUI

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+@plugin "daisyui";
 
 // https://vite.dev/config/
 export default defineConfig({
