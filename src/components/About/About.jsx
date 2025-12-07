364325cdc246c6cb76e9c0109c54db41e1fc9b17
ibrahimvau5518 <jibonm676@gmail.com>
2025-12-07 11:17:48 +0600

    added private route setup

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { FaQuestionCircle } from 'react-icons/fa';
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className='my-5'>
+      <h1 className="text-3xl text-center">Frequently asked questions</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 m-10 border-t pt-10">
+        <div>
+          <h1 className="font-bold flex items-center gap-1 mb-5">
+            <FaQuestionCircle />
+            How do I participate in contests on your platform?
+          </h1>
+          <p className="text-sm ">
+            To participate in a contest, simply navigate to the contest section
+            on our platform. Click on the contest you're interested in and
+            follow the on-screen instructions to submit your entry.
+          </p>
+        </div>
+        <div>
+          <h1 className="font-bold flex items-center gap-1 mb-5">
+            <FaQuestionCircle />
+            How do I participate in contests on your platform?
+          </h1>
+          <p className="text-sm ">
+            To participate in a contest, simply navigate to the contest section
+            on our platform. Click on the contest you're interested in and
+            follow the on-screen instructions to submit your entry.
+          </p>
+        </div>
+        <div>
+          <h1 className="font-bold flex items-center gap-1 mb-5">
+            <FaQuestionCircle />
+            How do I participate in contests on your platform?
+          </h1>
+          <p className="text-sm ">
+            To participate in a contest, simply navigate to the contest section
+            on our platform. Click on the contest you're interested in and
+            follow the on-screen instructions to submit your entry.
+          </p>
+        </div>
+        <div>
+          <h1 className="font-bold flex items-center gap-1 mb-5">
+            <FaQuestionCircle />
+            How do I participate in contests on your platform?
+          </h1>
+          <p className="text-sm ">
+            To participate in a contest, simply navigate to the contest section
+            on our platform. Click on the contest you're interested in and
+            follow the on-screen instructions to submit your entry.
+          </p>
+        </div>
+        
+      </div>
     </div>
   );
 };
